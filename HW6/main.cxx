780db10e847991df08435e07a6f5ae52b8df3687
German Luzin <germanluzin@gmail.com>
2024-10-10 15:58:17 +0200

    while loop -> for loop

--- HW6/main.cxx
+++ HW6/main.cxx
@@ -7,21 +7,26 @@ int main(int argc, char *argv[]) {
     char first_letter_of_1st_argument{*(argv[1])}; // *(argv[1])  would retrieve the first character of the 2nd word in input
     size_t program_name_lenght{program_name.size()};
     int second_argument{std::atoi(argv[2])}; // atoi converts second input into integer
-    auto ASCII_sum{0};
-    int counter{0}; //this variable works as an artificial counter for the while loop
+    long int ASCII_sum{0};// sum initial value is zero
+    // int counter{0}; //this variable works as an artificial counter for the while loop
      
     std::string first_argument{argv[1]}; // argv[1] would point to the string (to the 2nd word in input)
     
-    while (true) {    // this loop adds up all of the ASCII values of symbols in a first function argument 
-      ASCII_sum += first_argument[counter++];
-      if (counter >= static_cast<int>(first_argument.size())) {
-        break;
-      }
+    for(auto& i : first_argument){ // while loop rewritten
+      ASCII_sum += i;
     }
     if ((ASCII_sum ^ first_letter_of_1st_argument * 3) << (program_name_lenght & 0x1f) == second_argument) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
     }
+    //void key_sum(first_argument){
+    //  for(auto& i first_argument){
+    //    ASCII_sum += first_argument[i]
+    //  }
+    //}
+  }
+  else{
+    std::cout<< "Wrong number of arguments" << std::endl;
   }
 }
