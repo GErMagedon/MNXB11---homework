b4cde57dc73d75bcb2e7dc7ffc4d4ab6df80ce93
German Luzin <germanluzin@gmail.com>
2024-10-10 17:16:00 +0200

    Created function containing for loop

--- HW6/main.cxx
+++ HW6/main.cxx
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <string>
+
+/// @brief takes a string word as a parameter and sums ASCII values of it's letters
+/// @param word 
+/// @return returns the sum
+int sum_gen(std::string word){
+  int sum{0};
+  for(auto& i : word){ // while loop rewritten
+      sum += i;
+  }
+  return sum;
+}
+
 int main(int argc, char *argv[]) {
   bool correct_num_arg{argc==3};
   if (correct_num_arg) {
@@ -7,24 +19,15 @@ int main(int argc, char *argv[]) {
     char first_letter_of_1st_argument{*(argv[1])}; // *(argv[1])  would retrieve the first character of the 2nd word in input
     size_t program_name_lenght{program_name.size()};
     int second_argument{std::atoi(argv[2])}; // atoi converts second input into integer
-    long int ASCII_sum{0};// sum initial value is zero
-    // int counter{0}; //this variable works as an artificial counter for the while loop
-     
     std::string first_argument{argv[1]}; // argv[1] would point to the string (to the 2nd word in input)
     
-    for(auto& i : first_argument){ // while loop rewritten
-      ASCII_sum += i;
-    }
+    long int ASCII_sum = sum_gen(first_argument); // sum generated based on the keyword
+
     if ((ASCII_sum ^ first_letter_of_1st_argument * 3) << (program_name_lenght & 0x1f) == second_argument) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
     }
-    //void key_sum(first_argument){
-    //  for(auto& i first_argument){
-    //    ASCII_sum += first_argument[i]
-    //  }
-    //}
   }
   else{
     std::cout<< "Wrong number of arguments" << std::endl;
